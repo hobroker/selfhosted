d8ee270f0766013efeadb5b9a15f721e45bab07c
hobroker <igor.leahu24@gmail.com>
2020-11-30 04:49:14 +0200

    Add appdata_root variable
    Defaults to "/appdata"

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "jackett" {
   source = "./services/jackett"
 
   port           = 9117
-  config_path    = "/appdata/jackett"
+  config_path    = "${var.appdata_root}/jackett"
   blackhole_path = "/storage/downloads/torrents"
 }
 
@@ -16,7 +16,7 @@ module "qbittorrent" {
   source = "./services/qbittorrent"
 
   port           = 8112
-  config_path    = "/appdata/qbittorrent"
+  config_path    = "${var.appdata_root}/qbittorrent"
   blackhole_path = "/storage/downloads/torrents"
 }
 
@@ -25,7 +25,7 @@ module "sonarr" {
 
   tag            = "preview"
   port           = 8989
-  config_path    = "/appdata/sonarr"
+  config_path    = "${var.appdata_root}/sonarr"
   tv_path        = "/storage/tv-shows"
   downloads_path = "/storage/downloads"
 }
@@ -34,7 +34,7 @@ module "radarr" {
   source = "./services/radarr"
 
   port           = 7878
-  config_path    = "/appdata/radarr"
+  config_path    = "${var.appdata_root}/radarr"
   movies_path    = "/storage/movies"
   downloads_path = "/storage/downloads"
 }
@@ -43,10 +43,10 @@ module "code-server" {
   source = "./services/code-server"
 
   port        = 8084
-  config_path = "/appdata/code-server"
+  config_path = "${var.appdata_root}/code-server"
   mounts      = {
+    (var.appdata_root)     = var.appdata_root
     "/home/kira/compose" = "/compose"
-    "/appdata"           = "/appdata"
   }
 }
 
@@ -54,29 +54,29 @@ module "adguard" {
   source = "./services/adguard"
 
   port        = 3001
-  config_path = "/appdata/adguard/conf"
-  data_path   = "/appdata/adguard/work"
+  config_path = "${var.appdata_root}/adguard/conf"
+  data_path   = "${var.appdata_root}/adguard/work"
 }
 
 module "tautulli" {
   source = "./services/tautulli"
 
   port        = 8181
-  config_path = "/appdata/tautulli"
+  config_path = "${var.appdata_root}/tautulli"
 }
 
 module "xteve" {
   source = "./services/xteve"
 
   port        = 34400
-  config_path = "/appdata/xteve"
+  config_path = "${var.appdata_root}/xteve"
 }
 
 module "plex" {
   source = "./services/plex"
 
   port        = 32400
-  config_path = "/appdata/plex"
+  config_path = "${var.appdata_root}/plex"
   mounts      = {
     "/storage" = "/storage"
   }
@@ -86,5 +86,5 @@ module "ombi" {
   source = "./services/ombi"
 
   port        = 3579
-  config_path = "/appdata/ombi"
+  config_path = "${var.appdata_root}/ombi"
 }
