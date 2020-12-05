b9f75bdd55463e99f62e73451be4e4b2181f812e
hobroker <igor.leahu24@gmail.com>
2020-12-05 17:29:00 +0200

    Make traefik-labels optional

--- terraform/lib/traefik-labels/outputs.tf
+++ terraform/lib/traefik-labels/outputs.tf
@@ -1,8 +1,12 @@
-output "labels" {
-  value = {
+locals {
+  labels = {
     "subdomain"                                                  = var.name
     "traefik.http.routers.${var.name}.service"                   = var.name
     "traefik.http.services.${var.name}.loadbalancer.server.port" = var.port
     "traefik.docker.network"                                     = var.network
   }
+}
+
+output "labels" {
+  value = var.enabled ? local.labels : {}
 }
