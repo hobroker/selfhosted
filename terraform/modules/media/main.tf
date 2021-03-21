8842aef3e4f7adf7395b9969d669f16d6bc01792
Igor Leahu <igor.leahu24@gmail.com>
2021-03-21 17:14:00 +0200

    Remove unneded code

--- terraform/modules/media/main.tf
+++ terraform/modules/media/main.tf
@@ -8,39 +8,12 @@ resource "docker_network" "network" {
   driver = "overlay"
 }
 
-resource "docker_volume" "storage_volume" {
-  name        = "${local.name}-storage"
-  driver      = "local-persist"
-  driver_opts = {
-    mountpoint = var.storage_root
-  }
-}
-
-resource "docker_volume" "downloads_volume" {
-  name        = "${local.name}-downloads"
-  driver      = "local-persist"
-  driver_opts = {
-    mountpoint = "${var.storage_root}/downloads"
-  }
-}
-
 module "xteve" {
   source = "./xteve"
 
   port        = 34400
   network_ids = local.networks
   config_path = "${var.appdata_root}/xteve"
-}
-
-module "plex" {
-  source = "./plex"
-
-  port        = 32400
-  network_ids = local.networks
-  config_path = "${var.appdata_root}/plex"
-  volumes     = {
-    (docker_volume.storage_volume.name) = docker_volume.storage_volume.driver_opts.mountpoint
-  }
 }
 
 //module "ombi" {
