bc38e42ab482c5c858f99c2a1f18e09a09bcbc75
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2026-02-19 12:26:38 +0200

    fix(cli): add fallback in `useServiceStatus` for unknown local app version

--- packages/cli/src/components/ServiceItem/hooks/useServiceStatus.ts
+++ packages/cli/src/components/ServiceItem/hooks/useServiceStatus.ts
@@ -5,8 +5,10 @@ interface Props {
 }
 export function useServiceStatus({ service }: Props) {
   const isUpToDate =
-    service.installedChartVersion === service.localChartVersion &&
-    service.installedAppVersion === service.localAppVersion;
+    (service.installedChartVersion === service.localChartVersion &&
+      service.installedAppVersion === service.localAppVersion) ||
+    (service.localAppVersion === "unknown" &&
+      service.installedChartVersion === service.localChartVersion);
 
   if (!service.installedChartVersion) {
     return {
