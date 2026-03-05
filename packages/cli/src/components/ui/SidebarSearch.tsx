00619987ac585f22943b7fe82c80b77bbdd50674
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2026-03-06 00:05:46 +0200

    feat(cli): add search label in border of SidebarSearch

--- packages/cli/src/components/ui/SidebarSearch.tsx
+++ packages/cli/src/components/ui/SidebarSearch.tsx
@@ -6,16 +6,22 @@ interface Props {
 }
 
 export const SidebarSearch = ({ query }: Props) => {
+  const borderColor = query ? colors.primary : colors.dim;
   return (
     <Box
       borderStyle="single"
       borderLeft={false}
       borderRight={false}
       borderBottom={false}
-      borderColor={query ? colors.primary : colors.dim}
+      borderColor={borderColor}
       paddingX={1}
     >
-      <Text color={query ? colors.text : colors.dim}>{query || "/ search..."}</Text>
+      {query && (
+        <Box position="absolute" marginTop={-1} marginLeft={1}>
+          <Text color={borderColor}> SEARCH </Text>
+        </Box>
+      )}
+      <Text color={query ? colors.text : colors.dim}>{query || "/ type to search..."}</Text>
     </Box>
   );
 };
